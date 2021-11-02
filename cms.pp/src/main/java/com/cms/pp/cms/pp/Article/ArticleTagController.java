3248aa4c80f7a1e6f217e0fc66e3492bd0e4216f
mariski701 <mariusz.skiba1995@gmail.com>
2021-11-02 16:40:53 +0100

    endpointy na tagi

---
+++ cms.pp/src/main/java/com/cms/pp/cms/pp/Article/ArticleTagController.java
@@ -0,0 +1,37 @@
+package com.cms.pp.cms.pp.Article;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ArticleTagController {
+    @Autowired
+    ArticleTagService articleTagService;
+
+    @GetMapping("/api/tags")
+    public List<ArticleTag> getArticleTags () {
+        return articleTagService.getArticleTags();
+    }
+
+    @GetMapping("/api/tag/{id}")
+    public ArticleTag getArticleTag(@PathVariable int id) {
+        return articleTagService.getArticleTag(id);
+    }
+
+    @PostMapping("/api/tag/add")
+    public ArticleTag addTag (@RequestBody ArticleTag articleTag) {
+        return articleTagService.addTag(articleTag);
+    }
+
+    @DeleteMapping("/api/tag/remove/{id}")
+    public String removeTag(@PathVariable int id) {
+        return articleTagService.removeTag(id);
+    }
+
+    @PutMapping("/api/tag/modify/{id}")
+    public ArticleTag modifyTag(@RequestBody ArticleTag articleTag, @PathVariable int id) {
+        return articleTagService.modifyTag(id, articleTag);
+    }
+}
---
+++ cms.pp/src/main/java/com/cms/pp/cms/pp/Article/ArticleTagService.java
@@ -0,0 +1,41 @@
+package com.cms.pp.cms.pp.Article;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleTagService {
+    @Autowired
+    ArticleTagRepository articleTagRepository;
+
+    public List<ArticleTag> getArticleTags ()  {
+        return articleTagRepository.findAll();
+    }
+    public ArticleTag getArticleTag(int id) {
+        return articleTagRepository.findById(id).orElse(null);
+    }
+
+    public ArticleTag addTag(ArticleTag articleTag) {
+        return articleTagRepository.save(articleTag);
+    }
+
+    public String removeTag(int id) {
+        ArticleTag articleTag = articleTagRepository.findById(id).orElse(null);
+        if (articleTag == null) return "Error";
+        else {
+            articleTagRepository.delete(articleTag);
+            return "Tag of ID: " + articleTag.getId() +" and name "+articleTag.getName()+" is deleted";
+        }
+    }
+
+    public ArticleTag modifyTag(int id, ArticleTag articleTag) {
+        ArticleTag oldArticleTag = articleTagRepository.findById(id).orElse(null);
+        if (oldArticleTag == null) return null;
+        else {
+            oldArticleTag.setName(articleTag.getName());
+            return articleTagRepository.save(oldArticleTag);
+        }
+    }
+}
